64d6a5cc3fa66e6bba86a1b0f1056806002cb690
clpowe <clpowe@gmail.com>
2024-08-26 22:44:32 -0400

    move to ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config'
-import db from '@astrojs/db'
-import partytown from '@astrojs/partytown'
+import { defineConfig } from 'astro/config';
+import db from '@astrojs/db';
+import partytown from '@astrojs/partytown';
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [db(), partytown()]
-})
+  integrations: [db(), partytown()],
+  output: "server",
+  adapter: netlify()
+});
